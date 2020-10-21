7ca36a35627f80721e0a83b38cc4b8d153c2fa89
JackSnowdon <jacksnowdondrums@gmail.com>
2020-10-21 20:31:29 +0100

    accounts app imported and wired up

--- PopMaster/settings.py
+++ PopMaster/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'crispy_forms',
+    'home',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
